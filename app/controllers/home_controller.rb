c7332391cbd6a7b054065f3ea0ddf95097de7737
tamcv <chauviettam@gmail.com>
2017-06-15 23:37:15 +0700

    Add the sorting for menu items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,24 @@
 class HomeController < ApplicationController
-  def index
-  end
+    def index
+    end
 
-  def contact_us
-  end
+    def contact_us
+    end
  
     def menu
         @sections = Section.all
+        @sort_column = 'price'
+        @sort_direction = 'ascending'
         if params[:section_id].present?
             @current_section = Section.find(params[:section_id])
+            @food_items = @current_section.food_items
+        end
+
+        if params[:sort_column].present?
+            @sort_column = params[:sort_column]
+            @sort_direction = params[:sort_direction]
+            direction = params[:sort_direction] == 'ascending' ? 'ASC' : 'DESC'
+            @food_items = @food_items.order("#{params[:sort_column]} #{direction}")
         end
     end
 end
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -2,7 +2,7 @@ class FoodItem < ApplicationRecord
   belongs_to :section
   has_many :orders
   validates :name, :price, presence: true
-  
+
   def image_url_or_default
       image_url.presence || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQErz7bHX9p-uZrYdYY243uhhGYUQX0EtD9FaCxwtO-OxP5P-YgZA"
   end
